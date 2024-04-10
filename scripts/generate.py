93411d5cfb34c601f7840df2f24e41b25a2aa9ad
rafael <22560219+rmuller-ml@users.noreply.github.com>
2024-04-10 13:28:09 -0400

    remove cache_dir used for testing

--- scripts/generate.py
+++ scripts/generate.py
@@ -245,7 +245,7 @@ class HuggingfaceComplete(LLMGenerator):
         from transformers import AutoModelForCausalLM, AutoTokenizer
         import torch
         super().__init__(model)
-        self.hf_model = AutoModelForCausalLM.from_pretrained(model.name, device_map="cuda", torch_dtype=torch.bfloat16, cache_dir="/runs/cache")
+        self.hf_model = AutoModelForCausalLM.from_pretrained(model.name, device_map="cuda", torch_dtype=torch.bfloat16)
         if model.tokenizer:
             self.hf_tokenizer = AutoTokenizer.from_pretrained(model.tokenizer)
         else:
@@ -275,7 +275,7 @@ class HuggingfaceInfill(LLMGenerator):
         from transformers import AutoModelForCausalLM, AutoTokenizer
         import torch
         super().__init__(model)
-        self.hf_model = AutoModelForCausalLM.from_pretrained(model.name, device_map="cuda", torch_dtype=torch.bfloat16, cache_dir="/runs/cache")
+        self.hf_model = AutoModelForCausalLM.from_pretrained(model.name, device_map="cuda", torch_dtype=torch.bfloat16)
         if model.tokenizer:
             self.hf_tokenizer = AutoTokenizer.from_pretrained(model.tokenizer)
         else:
